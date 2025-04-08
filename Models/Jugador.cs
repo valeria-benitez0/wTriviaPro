a5e2378eaa932a3462232ab483fc04cad3454af3
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-04-08 16:02:36 -0500

    Se implementó la clase Jugador con métodos para gestionar respuestas correctas, incorrectas y reinicio del estado

---
+++ Models/Jugador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnsayoTrivia.Models
+{
+    public class Jugador
+    {
+        public string Nombre { get; private set; }
+        public int Puntaje { get; private set; }
+        public int PreguntasCorrectas { get; private set; }
+        public int PreguntasIncorrectas { get; private set; }
+
+        public Jugador(string nombre)
+        {
+            if (string.IsNullOrWhiteSpace(nombre))
+                throw new ArgumentException("El nombre del jugador no puede estar vacío.");
+
+            Nombre = nombre;
+            Puntaje = 0;
+            PreguntasCorrectas = 0;
+            PreguntasIncorrectas = 0;
+        }
+
+        public void AcertarPregunta(int puntos)
+        {
+            Puntaje += puntos;
+            PreguntasCorrectas++;
+        }
+
+        public void FallarPregunta()
+        {
+            Puntaje -= 2;
+            if (Puntaje < 0) Puntaje = 0;
+            PreguntasIncorrectas++;
+        }
+
+        public void Reiniciar()
+        {
+            Puntaje = 0;
+            PreguntasCorrectas = 0;
+            PreguntasIncorrectas = 0;
+        }
+    }
+}
---
+++ Models/Puntaje.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnsayoTrivia.Models
+{
+    public static class Puntaje
+    {
+        public static int CalcularPuntaje(Pregunta pregunta, bool esCorrecta)
+        {
+            if (esCorrecta)
+            {
+                return pregunta.ObtenerPuntaje();
+            }
+            else
+            {
+                return -2;
+            }
+        }
+    }
+}
