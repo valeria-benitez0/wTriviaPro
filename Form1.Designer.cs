2a81d6be212076e79b79625d04ec088e67c775cb
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-04-08 16:27:29 -0500

    Se conectó toda la lógica: generación de preguntas, evaluación de respuestas, retroalimentación inmediata, control de tiempo y puntaje de juego en formulario principal

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -78,7 +78,7 @@
             this.tableLayoutPanel1.Controls.Add(this.rbOpcion2, 0, 5);
             this.tableLayoutPanel1.Controls.Add(this.rbOpcion1, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.lblTiempo, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(14, 58);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 25);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 8;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.49953F));
@@ -89,53 +89,54 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.49953F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.49953F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.50328F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(490, 331);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(457, 403);
             this.tableLayoutPanel1.TabIndex = 18;
             // 
             // comboNivel
             // 
             this.comboNivel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.comboNivel.FormattingEnabled = true;
-            this.comboNivel.Location = new System.Drawing.Point(3, 51);
+            this.comboNivel.Location = new System.Drawing.Point(3, 64);
             this.comboNivel.Name = "comboNivel";
-            this.comboNivel.Size = new System.Drawing.Size(157, 21);
+            this.comboNivel.Size = new System.Drawing.Size(146, 21);
             this.comboNivel.TabIndex = 1;
             // 
             // btnResponder
             // 
             this.btnResponder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnResponder.Location = new System.Drawing.Point(166, 297);
+            this.btnResponder.Location = new System.Drawing.Point(155, 365);
             this.btnResponder.Name = "btnResponder";
-            this.btnResponder.Size = new System.Drawing.Size(157, 23);
+            this.btnResponder.Size = new System.Drawing.Size(146, 23);
             this.btnResponder.TabIndex = 8;
             this.btnResponder.Text = "Responder";
             this.btnResponder.UseVisualStyleBackColor = true;
+            this.btnResponder.Click += new System.EventHandler(this.btnResponder_Click);
             // 
             // lblPreguntasRestantes
             // 
             this.lblPreguntasRestantes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblPreguntasRestantes.AutoSize = true;
-            this.lblPreguntasRestantes.Location = new System.Drawing.Point(329, 55);
+            this.lblPreguntasRestantes.Location = new System.Drawing.Point(307, 68);
             this.lblPreguntasRestantes.Name = "lblPreguntasRestantes";
-            this.lblPreguntasRestantes.Size = new System.Drawing.Size(158, 13);
+            this.lblPreguntasRestantes.Size = new System.Drawing.Size(147, 13);
             this.lblPreguntasRestantes.TabIndex = 11;
             this.lblPreguntasRestantes.Text = "Preguntas: 15";
             // 
             // progressTiempo
             // 
             this.progressTiempo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressTiempo.Location = new System.Drawing.Point(166, 49);
+            this.progressTiempo.Location = new System.Drawing.Point(155, 63);
             this.progressTiempo.Name = "progressTiempo";
-            this.progressTiempo.Size = new System.Drawing.Size(157, 24);
+            this.progressTiempo.Size = new System.Drawing.Size(146, 24);
             this.progressTiempo.TabIndex = 13;
             // 
             // lblPuntaje
             // 
             this.lblPuntaje.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblPuntaje.AutoSize = true;
-            this.lblPuntaje.Location = new System.Drawing.Point(329, 14);
+            this.lblPuntaje.Location = new System.Drawing.Point(307, 18);
             this.lblPuntaje.Name = "lblPuntaje";
-            this.lblPuntaje.Size = new System.Drawing.Size(158, 13);
+            this.lblPuntaje.Size = new System.Drawing.Size(147, 13);
             this.lblPuntaje.TabIndex = 10;
             this.lblPuntaje.Text = "Puntaje: 0";
             // 
@@ -143,18 +144,19 @@
             // 
             this.comboCategoria.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.comboCategoria.FormattingEnabled = true;
-            this.comboCategoria.Location = new System.Drawing.Point(3, 10);
+            this.comboCategoria.Location = new System.Drawing.Point(3, 14);
             this.comboCategoria.Name = "comboCategoria";
-            this.comboCategoria.Size = new System.Drawing.Size(157, 21);
+            this.comboCategoria.Size = new System.Drawing.Size(146, 21);
             this.comboCategoria.TabIndex = 0;
+            this.comboCategoria.SelectedIndexChanged += new System.EventHandler(this.comboCategoria_SelectedIndexChanged);
             // 
             // rbOpcion4
             // 
             this.rbOpcion4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.rbOpcion4.AutoSize = true;
-            this.rbOpcion4.Location = new System.Drawing.Point(3, 302);
+            this.rbOpcion4.Location = new System.Drawing.Point(3, 370);
             this.rbOpcion4.Name = "rbOpcion4";
-            this.rbOpcion4.Size = new System.Drawing.Size(157, 13);
+            this.rbOpcion4.Size = new System.Drawing.Size(146, 13);
             this.rbOpcion4.TabIndex = 6;
             this.rbOpcion4.TabStop = true;
             this.rbOpcion4.UseVisualStyleBackColor = true;
@@ -162,20 +164,21 @@
             // btnIniciar
             // 
             this.btnIniciar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnIniciar.Location = new System.Drawing.Point(3, 91);
+            this.btnIniciar.Location = new System.Drawing.Point(3, 113);
             this.btnIniciar.Name = "btnIniciar";
-            this.btnIniciar.Size = new System.Drawing.Size(157, 23);
+            this.btnIniciar.Size = new System.Drawing.Size(146, 23);
             this.btnIniciar.TabIndex = 7;
             this.btnIniciar.Text = "Iniciar";
             this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
             // 
             // rbOpcion3
             // 
             this.rbOpcion3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.rbOpcion3.AutoSize = true;
-            this.rbOpcion3.Location = new System.Drawing.Point(3, 260);
+            this.rbOpcion3.Location = new System.Drawing.Point(3, 318);
             this.rbOpcion3.Name = "rbOpcion3";
-            this.rbOpcion3.Size = new System.Drawing.Size(157, 13);
+            this.rbOpcion3.Size = new System.Drawing.Size(146, 13);
             this.rbOpcion3.TabIndex = 5;
             this.rbOpcion3.TabStop = true;
             this.rbOpcion3.UseVisualStyleBackColor = true;
@@ -183,20 +186,21 @@
             // btnReiniciar
             // 
             this.btnReiniciar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnReiniciar.Location = new System.Drawing.Point(329, 297);
+            this.btnReiniciar.Location = new System.Drawing.Point(307, 365);
             this.btnReiniciar.Name = "btnReiniciar";
-            this.btnReiniciar.Size = new System.Drawing.Size(158, 23);
+            this.btnReiniciar.Size = new System.Drawing.Size(147, 23);
             this.btnReiniciar.TabIndex = 9;
             this.btnReiniciar.Text = "Reiniciar";
             this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
             // 
             // lblPregunta
             // 
             this.lblPregunta.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblPregunta.AutoSize = true;
-            this.lblPregunta.Location = new System.Drawing.Point(3, 137);
+            this.lblPregunta.Location = new System.Drawing.Point(3, 168);
             this.lblPregunta.Name = "lblPregunta";
-            this.lblPregunta.Size = new System.Drawing.Size(157, 13);
+            this.lblPregunta.Size = new System.Drawing.Size(146, 13);
             this.lblPregunta.TabIndex = 2;
             this.lblPregunta.Text = "Pregunta";
             // 
@@ -204,9 +208,9 @@
             // 
             this.rbOpcion2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.rbOpcion2.AutoSize = true;
-            this.rbOpcion2.Location = new System.Drawing.Point(3, 219);
+            this.rbOpcion2.Location = new System.Drawing.Point(3, 268);
             this.rbOpcion2.Name = "rbOpcion2";
-            this.rbOpcion2.Size = new System.Drawing.Size(157, 13);
+            this.rbOpcion2.Size = new System.Drawing.Size(146, 13);
             this.rbOpcion2.TabIndex = 4;
             this.rbOpcion2.TabStop = true;
             this.rbOpcion2.UseVisualStyleBackColor = true;
@@ -215,9 +219,9 @@
             // 
             this.rbOpcion1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.rbOpcion1.AutoSize = true;
-            this.rbOpcion1.Location = new System.Drawing.Point(3, 178);
+            this.rbOpcion1.Location = new System.Drawing.Point(3, 218);
             this.rbOpcion1.Name = "rbOpcion1";
-            this.rbOpcion1.Size = new System.Drawing.Size(157, 13);
+            this.rbOpcion1.Size = new System.Drawing.Size(146, 13);
             this.rbOpcion1.TabIndex = 3;
             this.rbOpcion1.TabStop = true;
             this.rbOpcion1.UseVisualStyleBackColor = true;
@@ -226,18 +230,18 @@
             // 
             this.lblTiempo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblTiempo.AutoSize = true;
-            this.lblTiempo.Location = new System.Drawing.Point(166, 14);
+            this.lblTiempo.Location = new System.Drawing.Point(155, 18);
             this.lblTiempo.Name = "lblTiempo";
-            this.lblTiempo.Size = new System.Drawing.Size(157, 13);
+            this.lblTiempo.Size = new System.Drawing.Size(146, 13);
             this.lblTiempo.TabIndex = 12;
             this.lblTiempo.Text = "Tiempo: 0s";
             // 
             // listHistorial
             // 
             this.listHistorial.FormattingEnabled = true;
-            this.listHistorial.Location = new System.Drawing.Point(520, 58);
+            this.listHistorial.Location = new System.Drawing.Point(488, 25);
             this.listHistorial.Name = "listHistorial";
-            this.listHistorial.Size = new System.Drawing.Size(250, 329);
+            this.listHistorial.Size = new System.Drawing.Size(300, 407);
             this.listHistorial.TabIndex = 17;
             // 
             // Form1
@@ -250,6 +254,7 @@
             this.Controls.Add(this.listHistorial);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using TriviaPro.Logica;
+using TriviaPro.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,199 @@ namespace TriviaPro
 {
     public partial class Form1 : Form
     {
+        private Pregunta preguntaActual;
+        private Jugador jugador;
+        private TemporizadorTrivia temporizador;
+        private int totalPreguntas = 15;
+        private int preguntasRespondidas = 0;
+
+        private List<RadioButton> opcionesRadioButtons;
         public Form1()
         {
             InitializeComponent();
+            InicializarComponentesPersonalizados();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InicializarComponentesPersonalizados()
+        {
+            // Iniciar combo boxes
+            comboCategoria.Items.AddRange(new string[] { "Ciencia", "Historia", "Cultura General" });
+            comboNivel.Items.AddRange(new string[] { "Fácil", "Medio", "Difícil" });
+
+            // Crear lista de RadioButtons
+            opcionesRadioButtons = new List<RadioButton> { rbOpcion1, rbOpcion2, rbOpcion3, rbOpcion4 };
+
+            // Iniciar timer lógico
+            temporizador = new TemporizadorTrivia();
+            temporizador.TiempoFinalizado += TiempoAgotado;
+            temporizador.TiempoActualizado += ActualizarTiempoVisual;
+
+            btnResponder.Enabled = false;
+            btnReiniciar.Enabled = false;
+        }
+        private void comboCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            if (comboCategoria.SelectedItem == null || comboNivel.SelectedItem == null)
+            {
+                MessageBox.Show("Por favor selecciona una categoría y un nivel.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            jugador = new Jugador("Jugador 1");
+            PreguntaFactory.ReiniciarPreguntas();
+            preguntasRespondidas = 0;
+            listHistorial.Items.Clear();
+            lblPuntaje.Text = "Puntaje: 0";
+            lblPreguntasRestantes.Text = $"Preguntas: {totalPreguntas - preguntasRespondidas}";
+
+            comboCategoria.Enabled = false;
+            comboNivel.Enabled = false;
+            btnIniciar.Enabled = false;
+            btnResponder.Enabled = true;
+            btnReiniciar.Enabled = true;
+
+            CargarSiguientePregunta();
+        }
+
+        private void CargarSiguientePregunta()
+        {
+            try
+            {
+                preguntaActual = PreguntaFactory.ObtenerPregunta(comboCategoria.SelectedItem.ToString(), comboNivel.SelectedItem.ToString());
+
+                lblPregunta.Text = preguntaActual.Texto;
+                var opciones = preguntaActual.Opciones.OrderBy(o => Guid.NewGuid()).ToList();
+
+                for (int i = 0; i < opcionesRadioButtons.Count; i++)
+                {
+                    if (i < opciones.Count)
+                    {
+                        opcionesRadioButtons[i].Text = opciones[i];
+                        opcionesRadioButtons[i].Visible = true;
+                        opcionesRadioButtons[i].Checked = false;
+                    }
+                    else
+                    {
+                        opcionesRadioButtons[i].Visible = false;
+                    }
+                }
+
+                temporizador.Iniciar(preguntaActual.ObtenerTiempo());
+                progressTiempo.Maximum = preguntaActual.ObtenerTiempo();
+                progressTiempo.Value = preguntaActual.ObtenerTiempo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar pregunta: " + ex.Message);
+            }
+        }
+
+        private void btnResponder_Click(object sender, EventArgs e)
+        {
+            string respuestaSeleccionada = opcionesRadioButtons.FirstOrDefault(rb => rb.Checked)?.Text;
+
+            if (respuestaSeleccionada == null)
+            {
+                MessageBox.Show("Selecciona una respuesta.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            temporizador.Detener();
+
+            bool correcta = preguntaActual.EvaluarRespuesta(respuestaSeleccionada);
+
+            if (correcta)
+            {
+                jugador.AcertarPregunta(preguntaActual.ObtenerPuntaje());
+                MessageBox.Show("¡Correcto!", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                jugador.FallarPregunta();
+                MessageBox.Show($"Incorrecto. La respuesta era: {preguntaActual.RespuestaCorrecta}", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            preguntasRespondidas++;
+            listHistorial.Items.Add($"{preguntasRespondidas}. {preguntaActual.Texto} - {(correcta ? "✅" : "❌")}");
+
+            lblPuntaje.Text = $"Puntaje: {jugador.Puntaje}";
+            lblPreguntasRestantes.Text = $"Preguntas: {totalPreguntas - preguntasRespondidas}";
+
+            if (preguntasRespondidas >= totalPreguntas)
+            {
+                FinalizarJuego();
+            }
+            else
+            {
+                CargarSiguientePregunta();
+            }
+        }
+
+        private void TiempoAgotado()
+        {
+            MessageBox.Show($"¡Se acabó el tiempo!\nLa respuesta era: {preguntaActual.RespuestaCorrecta}", "Tiempo Agotado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            jugador.FallarPregunta();
+
+            preguntasRespondidas++;
+            listHistorial.Items.Add($"{preguntasRespondidas}. {preguntaActual.Texto} - ⏱️");
+
+            lblPuntaje.Text = $"Puntaje: {jugador.Puntaje}";
+            lblPreguntasRestantes.Text = $"Preguntas: {totalPreguntas - preguntasRespondidas}";
+
+            if (preguntasRespondidas >= totalPreguntas)
+            {
+                FinalizarJuego();
+            }
+            else
+            {
+                CargarSiguientePregunta();
+            }
+        }
+
+        private void ActualizarTiempoVisual(int segundos)
+        {
+            lblTiempo.Text = $"Tiempo: {segundos}s";
+            progressTiempo.Value = segundos;
+        }
+
+        private void FinalizarJuego()
+        {
+            btnResponder.Enabled = false;
+            MessageBox.Show($"Juego finalizado.\nPuntaje final: {jugador.Puntaje}\nCorrectas: {jugador.PreguntasCorrectas}\nIncorrectas: {jugador.PreguntasIncorrectas}", "Resumen", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            comboCategoria.Enabled = true;
+            comboNivel.Enabled = true;
+            btnIniciar.Enabled = true;
+            btnResponder.Enabled = false;
+            btnReiniciar.Enabled = false;
+
+            lblPregunta.Text = "Pregunta";
+            foreach (var rb in opcionesRadioButtons)
+            {
+                rb.Text = "";
+                rb.Visible = false;
+            }
+
+            lblTiempo.Text = "Tiempo: 0s";
+            progressTiempo.Value = 0;
+            lblPuntaje.Text = "Puntaje: 0";
+            lblPreguntasRestantes.Text = "Preguntas: 15";
+            listHistorial.Items.Clear();
+        }
+
+
     }
 }
